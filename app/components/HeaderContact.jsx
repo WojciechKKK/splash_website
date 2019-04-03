edd082013c65f3d1e86233eabd68cc91522498f3
wojciechKKK <wojte_kk@wp.pl>
2019-04-03 21:11:43 +0200

    change-contact

--- app/components/HeaderContact.jsx
+++ app/components/HeaderContact.jsx
@@ -38,14 +38,15 @@ class HeaderContact extends Component{
         return(
             <div className="headerContact">
                 <div className="allContactElem">
-                    <div className="contactInfo"><img src="../image/address.png"></img>{text[1]}</div>
-                    <div className="contactInfo"><img src="../image/tel.png"></img>{text[2]}</div>
-                    <div className="contactInfo"><img src="../image/email2.png"></img><a style={{color: 'white'}}href="mailto:info@domain.com">{text[3]}</a></div>
+                    <div className="iframeAddress">
+                        <iframe className="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2443.6354178276733!2d21.003806315945223!3d52.23184126529641!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecc8c92692e49%3A0xc2e97ae5311f2dc2!2sPa%C5%82ac+Kultury+i+Nauki!5e0!3m2!1spl!2spl!4v1554315877464!5m2!1spl!2spl" style={{width: 400, height: 300,frameBorder:0, border: 0}} allowFullScreen></iframe>
+                        <div>{text[1]}<br />{text[2]}<br /><a style={{color: 'white'}}href="mailto:info@domain.com">{text[3]}</a></div>
+                    </div>
                 </div>
                 <div className="footer_formularz">
                     <div className="contactInfoHeader">{text[0]}</div>
                     <form onSubmit={this.sendForm}>
-                        <label>{textForm[0]}<br /><input onChange={this.handleSetName} value={valName} type="text" maxLength={15}></input></label>
+                        <label>{textForm[0]}<br /><input onChange={this.handleSetName} value={valName} type="text" maxLength={15} ></input></label>
                         <label>{textForm[1]} <br /><input onChange={this.handleSetEmail} value={valEmail} type="text" maxLength={25}></input></label>
                         <label>{textForm[2]}<br /><textarea onChange={this.handleSetMessage} value={valMessage} cols="40" rows="4" type="text" maxLength={50}></textarea>
                         <button className="contactBtn" type="submit">{textForm[3]}</button>
